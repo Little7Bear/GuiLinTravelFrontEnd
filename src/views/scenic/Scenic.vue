d658b23026b8298d2e39e962b812db4370b5b9dd
Little7Bear <2395373083@qq.com>
2020-03-28 01:11:06 +0800

    feat：景点首页

--- src/views/scenic/Scenic.vue
+++ src/views/scenic/Scenic.vue
@@ -1,16 +1,128 @@
 <template>
   <div class="scenic">
-    scenic
+    <!-- 表单 -->
+    <ul class="table">
+      <li class="table-item" v-for="(item, index) in table" :key="index">
+        <ImageCover :src="item.imgUrl" />
+        <div class="item-bottom">{{item.name}}</div>
+      </li>
+    </ul>
+
+    <!-- 分页 -->
+    <el-pagination
+      @current-change="handleCurrentChange"
+      :current-page="currentPage"
+      layout="total, prev, pager, next, jumper"
+      :page-size="pageSize"
+      :total="total"
+      background
+    ></el-pagination>
   </div>
 </template>
 
 <script>
-  export default {
+export default {
+  data () {
+    return {
+      table: [
+        {
+          id: '1',
+          imgUrl: 'http://n1-q.mafengwo.net/s6/M00/6B/3E/wKgB4lOUVU2AO9SCAASZJ6eRoz432.jpeg?imageMogr2%2Fthumbnail%2F%21192x130r%2Fgravity%2FCenter%2Fcrop%2F%21192x130%2Fquality%2F100',
+          name: '遇龙河'
+        },
+        {
+          id: '1',
+          imgUrl: 'http://n1-q.mafengwo.net/s6/M00/6B/3E/wKgB4lOUVU2AO9SCAASZJ6eRoz432.jpeg?imageMogr2%2Fthumbnail%2F%21192x130r%2Fgravity%2FCenter%2Fcrop%2F%21192x130%2Fquality%2F100',
+          name: '遇龙河'
+        },
+        {
+          id: '1',
+          imgUrl: 'http://n1-q.mafengwo.net/s6/M00/6B/3E/wKgB4lOUVU2AO9SCAASZJ6eRoz432.jpeg?imageMogr2%2Fthumbnail%2F%21192x130r%2Fgravity%2FCenter%2Fcrop%2F%21192x130%2Fquality%2F100',
+          name: '遇龙河'
+        },
+        {
+          id: '1',
+          imgUrl: 'http://n1-q.mafengwo.net/s6/M00/6B/3E/wKgB4lOUVU2AO9SCAASZJ6eRoz432.jpeg?imageMogr2%2Fthumbnail%2F%21192x130r%2Fgravity%2FCenter%2Fcrop%2F%21192x130%2Fquality%2F100',
+          name: '遇龙河'
+        },
+        {
+          id: '1',
+          imgUrl: 'http://n1-q.mafengwo.net/s6/M00/6B/3E/wKgB4lOUVU2AO9SCAASZJ6eRoz432.jpeg?imageMogr2%2Fthumbnail%2F%21192x130r%2Fgravity%2FCenter%2Fcrop%2F%21192x130%2Fquality%2F100',
+          name: '遇龙河'
+        },
+        {
+          id: '1',
+          imgUrl: 'http://n1-q.mafengwo.net/s6/M00/6B/3E/wKgB4lOUVU2AO9SCAASZJ6eRoz432.jpeg?imageMogr2%2Fthumbnail%2F%21192x130r%2Fgravity%2FCenter%2Fcrop%2F%21192x130%2Fquality%2F100',
+          name: '遇龙河'
+        },
+        {
+          id: '1',
+          imgUrl: 'http://n1-q.mafengwo.net/s6/M00/6B/3E/wKgB4lOUVU2AO9SCAASZJ6eRoz432.jpeg?imageMogr2%2Fthumbnail%2F%21192x130r%2Fgravity%2FCenter%2Fcrop%2F%21192x130%2Fquality%2F100',
+          name: '遇龙河'
+        },
+        {
+          id: '1',
+          imgUrl: 'http://n1-q.mafengwo.net/s6/M00/6B/3E/wKgB4lOUVU2AO9SCAASZJ6eRoz432.jpeg?imageMogr2%2Fthumbnail%2F%21192x130r%2Fgravity%2FCenter%2Fcrop%2F%21192x130%2Fquality%2F100',
+          name: '遇龙河'
+        },
+        {
+          id: '1',
+          imgUrl: 'http://n1-q.mafengwo.net/s6/M00/6B/3E/wKgB4lOUVU2AO9SCAASZJ6eRoz432.jpeg?imageMogr2%2Fthumbnail%2F%21192x130r%2Fgravity%2FCenter%2Fcrop%2F%21192x130%2Fquality%2F100',
+          name: '遇龙河'
+        },
+        {
+          id: '1',
+          imgUrl: 'http://n1-q.mafengwo.net/s6/M00/6B/3E/wKgB4lOUVU2AO9SCAASZJ6eRoz432.jpeg?imageMogr2%2Fthumbnail%2F%21192x130r%2Fgravity%2FCenter%2Fcrop%2F%21192x130%2Fquality%2F100',
+          name: '遇龙河'
+        }
+      ],
+      currentPage: 1,
+      pageSize: 10,
+      total: 100
+    }
+  },
 
+  methods: {
+    handleCurrentChange (val) {
+      console.log(val)
+    }
   }
+}
 
 </script>
 
 <style lang="scss" scoped>
+.table {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  justify-content: space-between;
+  grid-auto-flow: row dense;
+  grid-gap: 20px 10px;
+}
 
+.table-item {
+  height: 175px;
+  border: 1px solid $border-color-base;
+
+  .img-cover {
+    height: 130px;
+  }
+
+  .item-bottom {
+    text-align: center;
+    margin-top: 10px;
+    color: #1f1f1f;
+  }
+
+  &:hover {
+    .item-bottom {
+      color: $color-primary;
+    }
+  }
+}
+
+.el-pagination {
+  margin-top: 20px;
+  text-align: center;
+}
 </style>
